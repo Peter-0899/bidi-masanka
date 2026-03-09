cb8db62fdb1b8af6ebae098ed8bcb2a8538fd085
Peter-0899 <bidimasankapeters@gmail.com>
2026-03-09 16:33:16 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion des Contact</title>
+</head>
+<body>
+    <h2>MENU PRINCIPAL</h2>
+    <ul>
+        <li><a href="ajouter.php">Ajouter un contact</a></li>
+        <li><a href="afficher.php">Afficher les contact</a></li>
+    </ul>
+</body>
+</html>
