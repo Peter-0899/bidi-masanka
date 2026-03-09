2981d81e2932b29fea4307c08770ff5afe8eac6f
Peter-0899 <bidimasankapeters@gmail.com>
2026-03-09 16:22:37 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,8 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "BDContact";
+$conn = new mysqli($host,$user,$password,$dbname);
+if ($conn->connect_error) {die("Erreur de connexion : " .$conn->connect_error);}
+?>
